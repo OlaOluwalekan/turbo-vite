03be0597f66e02d69a7923e8b579c267efd38de4
OlaOluwalekan <olalekanbello534@gmail.com>
2025-11-20 14:25:33 +0100

    feat: initialize new documentation and web applications with Vite and React
    - Added Vite and React setup for the docs and web applications.
    - Created index.html and main.tsx files for both apps.
    - Implemented basic App component with typography for the web app.
    - Integrated Tailwind CSS for styling in both applications.
    - Added TypeScript configuration for both apps and helper packages.
    - Included SVG assets for Vite and React logos.
    - Established a typography component in the UI package for consistent text styling.
    - Updated turbo.json to include new output paths for build artifacts.

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@repo/ui/typography'
+
+const App = () => {
+  return (
+    <div>
+      <Typography element='h1'>App</Typography>
+    </div>
+  )
+}
+
+export default App
---
+++ packages/ui/src/typography.tsx
@@ -0,0 +1,62 @@
+import { CSSProperties, ReactNode } from 'react'
+
+export const Typography = ({
+  element,
+  children,
+  style,
+}: {
+  element: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p'
+  children: ReactNode
+  style?: CSSProperties
+}) => {
+  switch (element) {
+    case 'h1':
+      return (
+        <h1 style={{ fontSize: '32px', fontWeight: 900, ...style }}>
+          {children}
+        </h1>
+      )
+    case 'h2':
+      return (
+        <h2 style={{ fontSize: '28px', fontWeight: 800, ...style }}>
+          {children}
+        </h2>
+      )
+    case 'h3':
+      return (
+        <h3 style={{ fontSize: '24px', fontWeight: 700, ...style }}>
+          {children}
+        </h3>
+      )
+    case 'h4':
+      return (
+        <h4 style={{ fontSize: '20px', fontWeight: 700, ...style }}>
+          {children}
+        </h4>
+      )
+    case 'h5':
+      return (
+        <h5 style={{ fontSize: '18px', fontWeight: 600, ...style }}>
+          {children}
+        </h5>
+      )
+    case 'h6':
+      return (
+        <h6 style={{ fontSize: '16px', fontWeight: 600, ...style }}>
+          {children}
+        </h6>
+      )
+    case 'p':
+      return (
+        <p style={{ fontSize: '16px', fontWeight: 500, ...style }}>
+          {children}
+        </p>
+      )
+    default:
+      return (
+        <p style={{ fontSize: '16px', fontWeight: 500, ...style }}>
+          {children}
+        </p>
+      )
+  }
+}
